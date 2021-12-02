1953fe4d020545979432fc30e7524ecc4bc844bc
Askhat Zhamabayev <jam.askhat@gmail.com>
2021-12-02 23:39:43 +0600

    init: start rest-service task

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
-const { PORT } = require('./common/config');
-const app = require('./app');
+const fastify = require('fastify')({ logger: true })
 
-app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
-);
+fastify.register(require('./routes/users'))
+fastify.register(require('./routes/boards'))
+fastify.register(require('./routes/tasks'))
+
+const start = async () => {
+  try {
+    await fastify.listen(4000)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
