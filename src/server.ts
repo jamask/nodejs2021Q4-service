8a62b7295cbeb51ce0d6bf9bd58cce2549db936d
Askhat Zhamabayev <jam.askhat@gmail.com>
2021-12-19 23:07:06 +0600

    refactor: fix some problems

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,9 @@ fastify.register(require('./routes/users.ts'))
 fastify.register(require('./routes/boards.ts'))
 fastify.register(require('./routes/tasks.ts'))
 
+/**
+ * Create RESTful API server
+ */
 const start = async () => {
   try {
     await fastify.listen(4000)
