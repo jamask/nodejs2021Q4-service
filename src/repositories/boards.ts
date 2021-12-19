291e03e3f857faad88e8bf667be5bcaf7ed614fa
Askhat Zhamabayev <jam.askhat@gmail.com>
2021-12-19 12:09:41 +0600

    feat: convert routes from js into ts

---
+++ src/repositories/boards.ts
@@ -0,0 +1,38 @@
+const db = require('./db.ts')
+
+const selectBoards = () => db.boards
+
+const selectBoard = (id) => {
+  const returnBoard = db.boards.find((board) => board.id === String(id))
+
+  return returnBoard
+}
+
+const createBoard = (board) => {
+  db.boards.push(board)
+
+  return board
+}
+
+const changeBoard = (newBoard) => {
+  db.boards = db.boards.map((board) => (board.id === newBoard.id ? newBoard : board))
+  const returnBoard = { ...newBoard}
+  delete returnBoard.id
+
+  return returnBoard
+}
+
+const removeBoard = (id) => {
+  db.boards = db.boards.filter((board) => board.id !== String(id))
+  db.tasks = db.tasks.filter((task) => task.boardId !== String(id))
+
+  return { message: `Board ${id} has been removed` }
+}
+
+module.exports = {
+  selectBoards,
+  selectBoard,
+  createBoard,
+  changeBoard,
+  removeBoard,
+}
---
+++ src/services/boards.ts
@@ -0,0 +1,84 @@
+export {};
+const { v4: uuidv4 } = require('uuid')
+
+const {
+  selectBoards,
+  selectBoard,
+  createBoard,
+  changeBoard,
+  removeBoard,
+} = require('../repositories/boards.ts')
+
+const getBoards = (req, reply) => {
+  reply
+  .header('Content-Type', 'application/json; charset=utf-8')
+  .send(selectBoards())
+}
+
+const getBoard = (req, reply) => {
+  const { boardId } = req.params
+
+  const returnBoard = selectBoard(boardId)
+
+  if (returnBoard) {
+    reply
+    .code(200)
+    .header('Content-Type', 'application/json; charset=utf-8')
+    .send(returnBoard)
+  } else {
+    reply
+    .code(404)
+    .header('Content-Type', 'application/json; charset=utf-8')
+    .send({ message: `Board ${boardId} has not been found` })
+  }
+}
+
+const postBoard = (req, reply) => {
+  const { title, columns } = req.body
+
+  for (let i = 1; i < columns.length; i += 1) {
+    columns[i].id = uuidv4()
+  }
+
+  const newBoard = {
+    id: uuidv4(),
+    title,
+    columns,
+  }
+
+  reply
+    .code(201)
+    .header('Content-Type', 'application/json; charset=utf-8')
+    .send(createBoard(newBoard))
+}
+
+const updateBoard = (req, reply) => {
+  const { boardId } = req.params
+  const { title, columns } = req.body
+
+  const newBoard = {
+    id: boardId,
+    title,
+    columns,
+  }
+  
+  reply
+    .code(200)
+    .send(changeBoard(newBoard))
+}
+
+const deleteBoard = (req, reply) => {
+  const { boardId } = req.params
+
+  reply.send(removeBoard(boardId))
+}
+
+
+
+module.exports = {
+  getBoards,
+  getBoard,
+  postBoard,
+  updateBoard,
+  deleteBoard,
+}
