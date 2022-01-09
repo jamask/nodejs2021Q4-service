853f4816dc7f1759af58f91ee1b01c3dfbc845b8
Askhat Zhamabayev <jam.askhat@gmail.com>
2022-01-09 10:31:39 +0600

    init: start task 7

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,24 @@
 const fastify = require('fastify')({ logger: false })
 const logging = require('./log/index.ts')
+const Client = require('pg')
 
 fastify.register(require('./routes/users.ts'))
 fastify.register(require('./routes/boards.ts'))
 fastify.register(require('./routes/tasks.ts'))
 
 logging(fastify)
+
+const client = new Client({
+  password: 'postgres',
+  user: 'postgres',
+  host: 'postgres'
+})
+
+fastify.get('/pg-test', async function (request, reply) {
+  var data = await client.query("SELECT 1 + 1").then(() => "up").catch(() => "down");
+
+  reply.send(data)
+})
 
 /**
  * Create RESTful API server
